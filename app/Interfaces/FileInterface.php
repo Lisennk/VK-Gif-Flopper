f419b7737127d8b941b17fe8874168c41857d134
serewann <serewann@gmail.com>
2016-10-08 11:25:57 +0300

    Change dir struct and add file interface

---
+++ app/Interfaces/FileInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Lisennk\GifFlopper\Interfaces\FileInterface;
+
+/**
+ * Interface FileInterface
+ * @package Lisennk\GifFlopper\Interfaces\FileInterface
+ */
+interface FileInterface
+{
+    public function getBlob(): string;
+}

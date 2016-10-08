6911034d196901a8211a7a664e1963ee6aa19e01
serewann <serewann@gmail.com>
2016-10-08 12:17:16 +0300

    Add documentation

--- app/Gif.php
+++ app/Gif.php
@@ -5,17 +5,33 @@ namespace Lisennk\GifFlopper;
 use Lisennk\GifFlopper\Interfaces\FileInterface;
 use Imagick;
 
+/**
+ * Class Gif
+ * @package Lisennk\GifFlopper
+ */
 class Gif implements FileInterface
 {
+    /**
+     * Image
+     *
+     * @var Imagick
+     */
     protected $gif;
-    protected $api;
 
-
+    /**
+     * Gif constructor.
+     * @param string $url
+     */
     public function __construct(string $url)
     {
         $this->gif = (new Imagick($url))->coalesceImages();
     }
 
+    /**
+     * Flop is "horizontal mirror" of image
+     *
+     * @return Gif
+     */
     public function flop(): self
     {
         foreach ($this->gif as $frame) {
@@ -25,6 +41,11 @@ class Gif implements FileInterface
         return $this;
     }
 
+    /**
+     * Returns files binary content
+     *
+     * @return string
+     */
     public function getBlob(): string
     {
         $this->gif->deconstructImages();
--- app/VK/Document.php
+++ app/VK/Document.php
@@ -6,33 +6,72 @@ use Lisennk\GifFlopper\Interfaces\FileInterface;
 use VK\VK;
 use GuzzleHttp\Client;
 
+/**
+ * Class Document
+ * @package Lisennk\GifFlopper\VK
+ */
 class Document
 {
-    protected $vk;
-    protected $client;
-    protected $name;
-    protected $content;
+    /**
+     * @var VK
+     */
+    protected $api;
 
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    protected $client;
+
+    /**
+     * File blob
+     *
+     * @var string
+     */
+    protected $blob;
+
+    /**
+     * Document constructor.
+     * @param string $filename
+     * @param FileInterface $file
+     * @param VK $api
+     */
     public function __construct(string $filename, FileInterface $file, VK $api)
     {
-        $this->vk = $api;
+        $this->api = $api;
         $this->client = new Client();
-        $this->content = $file->getBlob();
+        $this->blob = $file->getBlob();
         $this->saveAs($filename);
     }
 
+    /**
+     * Save doc on vk.com as $filename
+     *
+     * @param string $filename
+     * @return string Uploaded Doc Url
+     */
     protected function saveAs(string $filename): string
     {
         $uploadUrl = $this->getUploadUrl();
-        $file = $this->uploadFile($uploadUrl, $filename, $this->content);
+        $file = $this->uploadFile($uploadUrl, $filename, $this->blob);
         return $this->saveFile($file);
     }
 
+    /**
+     * @return string Url where to upload document
+     */
     protected function getUploadUrl(): string
     {
-        return $this->vk->api('docs.getUploadServer')['response']['upload_url'];
+        return $this->api->api('docs.getUploadServer')['response']['upload_url'];
     }
 
+    /**
+     * Perform file upload
+     *
+     * @param string $url
+     * @param string $filename
+     * @param string $content
+     * @return string
+     */
     protected function uploadFile(string $url, string $filename, string $content): string
     {
         $response = $this->client->request('POST', $url, [
@@ -49,9 +88,15 @@ class Document
         return json_decode($response)->file;
     }
 
+    /**
+     * Saves uploaded doc on vk.com
+     *
+     * @param $file
+     * @return string
+     */
     protected function saveFile($file): string
     {
-        return $this->vk->api('docs.save', [
+        return $this->api->api('docs.save', [
             'file' => $file,
         ])['response'][0]['url'];
     }
